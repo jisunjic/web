f6bef275d8dcd540309671410fa67b33fdb34027
jisunjic <146954995+jisunjic@users.noreply.github.com>
2023-10-24 03:18:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let menu=document.querySelector("#menuicon");
+let navbar=document.querySelector(".navbar");
+
+
+menu.onclick=()=>{
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("open")
+}
